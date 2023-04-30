26f3c1eb7910835ec2ad461ebc2c086a9fb64c85
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-04-30 17:32:44 +0800

    feat: refine layout

--- src/Components/ShowAllBoards/ShowAllBoards.tsx
+++ src/Components/ShowAllBoards/ShowAllBoards.tsx
@@ -1,6 +1,6 @@
 import ShowCards from "../ShowCards/ShowCards";
 import Hand from "../../views/Deal/Hand";
-
+import "./index.css"
 interface ShowAllBoardsProps {
   all_boards: Hand[][];
 }
@@ -8,8 +8,8 @@ interface ShowAllBoardsProps {
 export default function ShowAllBoards(props: ShowAllBoardsProps) {
   const { all_boards } = props;
   return (
-    <>
+    <div className="all-boards">
       {all_boards.map((board: Hand[], key) => <ShowCards key={key} all_hands={board} />)}
-    </>
+    </div>
   )
 }
