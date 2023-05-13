85f412088088c84d2aa5ea0e3148b7e0da75d82c
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-05-13 16:48:20 +0800

    feat: add color for this round

--- src/Components/HandSetting/KnownOneCards.tsx
+++ src/Components/HandSetting/KnownOneCards.tsx
@@ -1,11 +1,11 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { NUMBER2COLOR, RANK2CARD } from "../../Utils/maps";
 import { DealContext } from "../../views/Deal/Deal";
 import { HandSettingContext } from "./HandSetting";
 
 export default function KnownOneCards() {
   const [show, setShow] = useState<boolean>(false);
-  let to_deal_cards: number[] = [];
+  const [to_deal_cards, setTo_deal_cards] = useState(new Set<number>());
   const deal_context = useContext(DealContext);
 
   function handleClick() {
@@ -13,18 +13,26 @@ export default function KnownOneCards() {
   }
 
   function handleCardClick(card: number, e: any) {
-    to_deal_cards.push(card);
+    to_deal_cards.add(card);
+    setTo_deal_cards(new Set(to_deal_cards.add(card)));
   }
 
   function handleSubmit() {
     to_deal_cards.forEach((card) => deal_context.known_cards[card] === 0 ? deal_context.known_cards[card] = 1 : deal_context.known_cards[card] = 0);
+    to_deal_cards.clear();
+    setTo_deal_cards(to_deal_cards);
     setShow(false);
   }
 
   function handleCancel() {
-    to_deal_cards = [];
+    to_deal_cards.clear();
+    setTo_deal_cards(to_deal_cards);
     setShow(false);
   }
+
+  useEffect(() => {
+    console.log(to_deal_cards)
+  }, [to_deal_cards]);
 
   return (
     <DealContext.Consumer>
@@ -43,7 +51,8 @@ export default function KnownOneCards() {
                           <div>{color}  {RANK2CARD.map((val, idx) =>
                             <button onClick={(e) => handleCardClick(13 * key + idx, e)}
                               disabled={context1.known_cards[13 * key + idx] !== 1 && context1.known_cards[13 * key + idx] !== 0}
-                              className={context1.known_cards[13 * key + idx] ? "known" : "unknown"}>{val}
+                              className={(context1.known_cards[13 * key + idx] ? "known" : "unknown") + " " + (to_deal_cards.has(13 * key + idx) ? "now" : "")}>
+                              {val}
                             </button>)}
                           </div>
                         )
