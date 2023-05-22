84126141b0994578a5e0b84d5cffdf372a2ef948
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-05-22 23:17:22 +0800

    feat: add new items in sidebar

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -9,7 +9,9 @@ export default function Sidebar() {
         <li><Link to="deal">Deal With One Hand Setting</Link></li>
         <li><Link to="deal-multi">Deal With Multi Hands Setting</Link></li>
         <li><Link to="analysis">Analysis</Link></li>
-
+        <li>imp to vp</li>
+        <li>Calculate points of contract</li>
+        <li><a href="https://bridge.esmarkkappel.dk/main/main.html" target="_blank">单套结构打法</a></li>
       </ul>
     </nav>
 
