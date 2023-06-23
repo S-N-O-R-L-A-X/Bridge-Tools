4a0925d2bbc6b3d30095f9d12729fb99d8fb6438
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-06-23 21:47:07 +0800

    feat: add loading

--- src/Components/PlayBoard/PlayBoard.tsx
+++ src/Components/PlayBoard/PlayBoard.tsx
@@ -1,9 +1,17 @@
 import "./PlayBoard.css";
+import { useState } from "react";
 
 export default function PlayBoard() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  function handleOnload() {
+    setIsLoading(false);
+  }
+
   return (
     <>
-      <iframe id="bridgeweb-board" name="myiframe" src="https://dds.bridgewebs.com/bridgesolver/upload.htm" style={{ "display": "block" }}></iframe>
+      <iframe onLoad={handleOnload} id="bridgeweb-board" name="myiframe" src="https://dds.bridgewebs.com/bridgesolver/upload.htm"></iframe>
+      {isLoading && "Please wait for several seconds..."}
       <p>This page is from <a href="https://dds.bridgewebs.com/bridgesolver/upload.htm">https://dds.bridgewebs.com/bridgesolver/upload.htm</a></p>
     </>
   )
