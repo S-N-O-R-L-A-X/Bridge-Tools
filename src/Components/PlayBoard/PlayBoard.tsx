a5b918c007c96bd560a5ca8fec5ff06f0215f300
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-06-28 23:21:05 +0800

    feat: add loading in play board

--- src/Components/PlayBoard/PlayBoard.tsx
+++ src/Components/PlayBoard/PlayBoard.tsx
@@ -10,8 +10,8 @@ export default function PlayBoard() {
 
   return (
     <>
+      {isLoading && <h3>Please wait for several seconds...</h3>}
       <iframe onLoad={handleOnload} id="bridgeweb-board" name="myiframe" src="https://dds.bridgewebs.com/bridgesolver/upload.htm"></iframe>
-      {isLoading && "Please wait for several seconds..."}
       <p>This page is from <a href="https://dds.bridgewebs.com/bridgesolver/upload.htm">https://dds.bridgewebs.com/bridgesolver/upload.htm</a></p>
     </>
   )
