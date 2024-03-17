e985bf9da5725c7ecdc6638562520112cf3975af
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-03-17 23:39:56 +0800

    feat: add contract buttons

--- src/views/CalculateContract/CalculateContract.tsx
+++ src/views/CalculateContract/CalculateContract.tsx
@@ -1,6 +1,15 @@
+import { CONTRACTCOLORS } from "../../Utils/maps";
+
 export default function CalculateContract() {
+	const others = ["PASS", "X", "XX"];
 	return (
 		<>
+			{new Array(7).fill(0).map((_, idx) => idx + 1).map((num) =>
+				<div>
+					{CONTRACTCOLORS.map((color) => <button key={num + color}>{num}{color}</button>)}
+				</div>
+			)}
+			{others.map((text) => <button key={text}>{text}</button>)}
 		</>
 	)
 }
