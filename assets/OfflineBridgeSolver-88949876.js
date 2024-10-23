579b627c085580d8d84b7aa3b38a164c7156df92
S-N-O-R-L-A-X <snorlax.xu@outlook.com>
2024-10-23 14:02:23 +0000

    Deploying to gh-pages from  @ ef5ea10c116b6f75cbe95bff5cb74b3ab2a0072f 🚀

---
+++ assets/OfflineBridgeSolver-88949876.js
@@ -0,0 +1,1 @@
+import{r as t,a as o,j as f,S as i}from"./index-0f25ba11.js";function l(e){const{allHands:s=[]}=e,[r,a]=t.useState();return t.useEffect(()=>{o(s).then(n=>{a(n)})},[s]),f.jsx(i,{ddtricks:r})}export{l as default};
